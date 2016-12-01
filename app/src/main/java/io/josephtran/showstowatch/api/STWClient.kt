a74ad9691a976462ae956b32ae45c567765272d9
jotran <josephthientran@gmail.com>
2016-12-01 00:12:49 -0800

    Increase timeout of STWClient
    Timeout was increased by building an OkHttpClient with a timeout of 60
    seconds for connect, read, and write.
    The timeout needed to be increased because showstowatch.josephtran.io
    is using heroku's free plan and therefore falls asleep after 30 mins of
    inactivity.
    
    Update build.gradle to use okhttp, so that OkHttpClients could be
    created

--- app/src/main/java/io/josephtran/showstowatch/api/STWClient.kt
+++ app/src/main/java/io/josephtran/showstowatch/api/STWClient.kt
@@ -1,16 +1,24 @@
 package io.josephtran.showstowatch.api
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
 public class STWClient {
     private val stwService: STWService;
     private val stwUrl = "http://showstowatch.josephtran.io/"
 
     init {
+        val httpClient = OkHttpClient().newBuilder()
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .readTimeout(60, TimeUnit.SECONDS)
+                .writeTimeout(60, TimeUnit.SECONDS)
+                .build();
         val retrofit = Retrofit.Builder()
-                .addConverterFactory(MoshiConverterFactory.create())
                 .baseUrl(stwUrl)
+                .client(httpClient)
+                .addConverterFactory(MoshiConverterFactory.create())
                 .build()
         stwService = retrofit.create(STWService::class.java)
     }
