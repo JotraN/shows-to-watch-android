9a4893ccf1388fc2f67880aac07c1ec811df7b42
jotran <josephthientran@gmail.com>
2017-01-11 23:49:08 -0800

    Make STWShow a Parcelable

--- app/src/main/java/io/josephtran/showstowatch/api/STWShow.kt
+++ app/src/main/java/io/josephtran/showstowatch/api/STWShow.kt
@@ -1,5 +1,7 @@
 package io.josephtran.showstowatch.api
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.squareup.moshi.Json
 
 data class STWShow(
@@ -16,4 +18,31 @@ data class STWShow(
         val tvdbId: String = "",
         val abandoned: Boolean = false,
         val completed: Boolean = false
-)
+) : Parcelable {
+    companion object {
+        @JvmField val CREATOR: Parcelable.Creator<STWShow> = object : Parcelable.Creator<STWShow> {
+            override fun createFromParcel(source: Parcel): STWShow = STWShow(source)
+            override fun newArray(size: Int): Array<STWShow?> = arrayOfNulls(size)
+        }
+    }
+
+    constructor(source: Parcel) : this(
+            source.readInt(), source.readString(), source.readInt(), source.readInt(),
+            source.readString(), source.readString(), source.readString(), source.readString(),
+            1 == source.readInt(), 1 == source.readInt())
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel?, flags: Int) {
+        dest?.writeInt(id!!)
+        dest?.writeString(title)
+        dest?.writeInt(season)
+        dest?.writeInt(episode)
+        dest?.writeString(createdAt)
+        dest?.writeString(updatedAt)
+        dest?.writeString(banner)
+        dest?.writeString(tvdbId)
+        dest?.writeInt((if (abandoned) 1 else 0))
+        dest?.writeInt((if (completed) 1 else 0))
+    }
+}
